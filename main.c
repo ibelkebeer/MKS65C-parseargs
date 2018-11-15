eb151a114e5572788ebf8e02f225ca61ffeac2a4
Calvin Aw <caw@stuy.edu>
2018-11-14 19:31:41 -0500

    yayz

--- main.c
+++ main.c
@@ -4,20 +4,37 @@
 #include <string.h>
 
 char ** parse_args(char* line){
-  char** ary= calloc(6, sizeof(char*));
-  int i;
-  for(i = 0; i < 6; i++){
-    char sep[] = line;
-    printf("%s\n", sep);
-    ary[i] = strsep(&line, " ");
+  char** ary = calloc(6, sizeof(char*));
+  int i = 0;
+  char * k = "a";
+  char * m = " ";
+  int ctr = 0;
+  for(i; i < 6; i++){
+    if (m == k && ctr != 1){
+      ary[i] = m;
+      ctr ++;
+    }
+    if (m == k)
+      ary[i] = NULL;
+    if (m != k){
+      k = strsep(&line, " ");
+      ary[i] = k;
+    }
+    m = line;
+    printf("in array[%d]: %s\n", i, ary[i]);
+    printf("still need to parse: %s\n", line);
   }
+  
   return ary;
 }
 
+
 int main(){
-  char* input = "ls -l";
+  char ex[100] = "ls -a -l";
+  char * input = ex;
   char** command;
   command = parse_args(input);
+  
   execvp(command[0], command);
   return 0;
 }
--- makefile
+++ makefile
@@ -4,5 +4,5 @@ all: main.o
 main.o: main.c main.h 
 	gcc -c main.c
 
-run: a.out
+run:
 	./a.out
